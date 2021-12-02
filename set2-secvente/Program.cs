5e02ae2333d8ade36154ec06efed25c71009f724
Vinter Andrada <vinterandrada08@gmail.com>
2021-12-02 20:55:40 +0200

    fara pb. 13,14,15,16

---
+++ set2-secvente/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace set2_secvente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int optiune;
+            Console.WriteLine("Alegeti o optiune:");
+            optiune = int.Parse(Console.ReadLine());
+        }
+    }
+}
